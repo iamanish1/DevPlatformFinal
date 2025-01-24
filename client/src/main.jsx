20f077a317ea8c738303063f2cb69a4c11addca7
iamanish1 <animetra14@gmail.com>
2025-01-24 23:12:33 +0530

    xy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from 'react-router-dom'
-import createRouter from './routes/index.jsx' ; 
+import router from './routes/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={createRouter} />
+    <RouterProvider router={router} />
      
   </StrictMode>,
 )
