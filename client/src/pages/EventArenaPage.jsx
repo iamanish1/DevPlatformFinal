d9f78f7181e25effa9f6e0b697f4e73dfd13b745
iamanish1 <animetra14@gmail.com>
2025-02-02 20:15:01 +0530

    done with the al routes of the event pages also

---
+++ client/src/pages/EventArenaPage.jsx
@@ -0,0 +1,9 @@
+const EventArenaPage = ()=>{
+    return (
+        <>
+        this is my event arena page 
+        </>
+    )
+}; 
+
+export default EventArenaPage;
--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -13,6 +13,12 @@ import BugHuntArena from "../pages/BugHuntArena";
 import ProtectedRoute from "../components/ProtectedRoute";
 import { AuthContext}  from "../Context/AuthContext";
 import { useContext } from "react";
+import EventArenaPage from "../pages/EventArenaPage";
+import HackthonPage from "../pages/HackthonPage";
+import DebuggingduelPage from "../pages/DebuggingDuelPage";
+import CodeBattelPage from "../pages/CodeBattelPage";
+import SpeedGuidnace from "../pages/SpeedGuidnace";
+import QuizPage from "../pages/QuizPage";
 
 const AppRoutes = () => {
     const isAuthenticated = useContext(AuthContext); 
@@ -26,7 +32,14 @@ const AppRoutes = () => {
         <Route path="/Carrier" element={<CarrierSection/>} />
         <Route path="/Practice" element={<ProblemSection/>} />
         <Route path="/Profile" element={<ProfilePage/>} />
-        <Route path="/BugHuntArena" element={<BugHuntArena/>} />
+        <Route path="/participant/Eventarena" element={<EventArenaPage/>} />
+        <Route path="participant/bughunt"  element={<BugHuntArena/>} />
+        <Route path="participant/Hackthon" element={<HackthonPage/>}/>
+        <Route path="participant/debuggingduel" element={<DebuggingduelPage/>} />
+        <Route path="participant/codebattel" element={<CodeBattelPage/>} />
+        <Route path="participant/speedguide" element={<SpeedGuidnace/>}/>
+        <Route path="participant/Quiz" element={<QuizPage/>} />
+
         </Route>
         <Route path="/" element={<HomePage/>} />
         <Route path="/CreateAccount" element={<CreateAccount/>} />
