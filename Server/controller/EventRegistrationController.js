6320516cc48d2a33f1b02f107dd3b5894751dafa
iamanish1 <animetra14@gmail.com>
2025-02-20 20:42:23 +0530

    done with the send email function for email confirmation for the register participant .

---
+++ Server/controller/EventRegistrationController.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer" ; 
+
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+    },
+});
+
+// Function to send email
+const sendConfirmationEmail = async (email , eventTitel)=> {
+       const mailoptions = {
+        from: process.env.GMAIL_USERNAME,
+        to: email,
+        subject: "Event Registration Confirmation",
+        text: `You have successfully registered for the event: ${eventTitle}. See you there!`,
+       }
+
+       await transporter.sendMail(mailoptions)
+       console.log("Email sent successfully")
+}
+
+export default sendConfirmationEmail;  // Export the function for use in other parts of the application.
